02245849b813ab774dcb617552c069b65d2ff228
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-05-17 20:56:03 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int parent[102];
+
+int getParent(int a) {
+    if (parent[a] == a) return a;
+    else return parent[a] = getParent(parent[a]);
+}
+
+void unionParent(int a, int b) {
+    int x = getParent(a);
+    int y = getParent(b);
+    
+    if (x != y) {
+        if (x > y) {
+            parent[x] = y;
+        } else {
+            parent[y] = x;
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    for (int i = 1; i <= n; i++)
+        parent[i] = i;
+        
+    vector<tuple<int, int, int>> v;
+    
+    for (int i = 0; i < costs.size(); i++) {
+        v.push_back({costs[i][2], costs[i][0], costs[i][1]});
+    }
+    
+    sort(v.begin(), v.end());
+    
+    int len = 0;
+    for (int i = 0; i < v.size(); i++) {
+        if (len == n - 1) {
+            break;
+        }
+        int dist, a, b;
+        tie(dist, a, b) = v[i];
+        if (getParent(a) != getParent(b)) {
+            unionParent(a, b);
+            answer += dist;
+            len++;
+        }
+    }
+    
+    return answer;
+}
