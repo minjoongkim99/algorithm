4adf73a3c2e6765f84f12390dfc6542695a4b306
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-05-19 16:35:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.65 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,30 +1,27 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
+
 using namespace std;
 
 string solution(vector<string> p, vector<string> c) {
-    ios_base::sync_with_stdio(false);
     
     string answer = "";
-    unordered_map<string, int> m;
     
-    for(int i = 0; i < p.size(); i++){
-        m[p[i]]++;
+    unordered_map <string, int> m;
+    for(const string &s : p){
+        m[s]++;
     }
     
-    for(int i = 0; i < c.size(); i++){
-        m[c[i]]--;
-    }
-    
-    for(auto e : m){
-        if(e.second > 0){
-            answer = e.first;
-            break;
+    for(const string &s : c){
+        if(m[s] > 0){
+            m[s]--;
         }
-        else continue;
+        if(m[s] == 0){
+            m.erase(s);
+        }
     }
     
+    answer = m.begin()->first;
     return answer;
 }
