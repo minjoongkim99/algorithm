97d32d566dcd9db133de6d910ee9b6be3ba924a2
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-05-22 21:37:54 +0900

    [level 1] Title: 실패율, Time: 0.27 ms, Memory: 5.16 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.cpp
+++ 프로그래머스/1/42889. 실패율/실패율.cpp
@@ -14,30 +14,29 @@ bool cmp(pair<int,float> & a, pair<int,float> &b){
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
     vector<int> arr(N + 2, 0);
+    
     vector<pair<int, float>> v;
     for(auto e : stages){
         arr[e]++;
     }
     
- 
     int len = stages.size();
-
     for(int i = 1; i < arr.size() - 1; i++){
-        if(len == 0){
+        if(len <= 0){
             v.push_back({i, 0});
             continue;
         }
-        float failure = (float)arr[i] / (float)len;
-        v.push_back({i, failure});
-        len -= arr[i];
+        else{
+            float failure = (float)arr[i] / (float)len;
+            v.push_back({i, failure});
+            len -= arr[i];
+        }
     }
     
     sort(v.begin(), v.end(), cmp);
     for(auto e : v){
-        //cout << v[i].first << " " << v[i].second << "\n";
         answer.push_back(e.first);
     }
-    
-    
+
     return answer;
 }
