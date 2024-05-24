4abe73b50b154a8e0419bc2ed103491c2b37b8d8
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-05-24 10:31:42 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 284 ms, Memory: 16316 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);  cout.tie(NULL);
+    unordered_map<string, string > db;
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        string site, pwd;
+        cin >> site >> pwd;
+        db[site] = pwd;
+    }
+
+    for(int i = 0; i < m; i++){
+        string site;
+        cin >> site;
+        if(db.find(site) != db.end())
+            cout << db[site] << "\n";
+    }
+    return 0;
+}
