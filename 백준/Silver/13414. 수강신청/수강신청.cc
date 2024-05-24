7f8a7f9782d1784ec1e022db3fac22a5ec3080c0
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-05-24 11:57:32 +0900

    [Silver III] Title: 수강신청, Time: 168 ms, Memory: 35432 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.cc
+++ 백준/Silver/13414. 수강신청/수강신청.cc
@@ -7,32 +7,25 @@ using namespace std;
 
 int k, l;
 int main(){
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
     cin >> k >> l;
     unordered_map<string ,int> m;
 
     for(int i = 0; i < l; i++){
         string name;
         cin >> name;
-        if(m.find(name) != m.end()){
-            m.erase(name);
-            m[name] = i;
-        }
-        else m[name] = i;
+        m[name] = i;
     }
 
     vector<pair<int,string>> v;
 
     for(auto e: m){
-        //cout << e.first << " " << e.second << "\n";
         v.push_back({e.second, e.first});
     }
 
     sort(v.begin(), v.end());
-    for(auto e : v){
-        //cout << e.first << " " << e.second << "\n";
-    }
-
+   
     int len = min(k, (int)v.size());
     for(int i = 0; i < len; i++){
         cout << v[i].second << '\n';
