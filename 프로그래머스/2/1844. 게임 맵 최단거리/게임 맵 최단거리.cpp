89fbc7ac60aea463e45a3a91abf6231c22680310
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-07-03 18:01:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.22 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,44 +1,53 @@
 #include<vector>
-#include<queue>
 #include<iostream>
+#include<queue>
 using namespace std;
 
+int n, m;
+int arr[102][102];
 int visited[102][102];
-int dy[4] = {-1,0,1,0};
-int dx[4] = {0,-1,0,1};
 
-int solution(vector<vector<int> > maps)
-{
-    ios_base::sync_with_stdio(false);
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+
+void bfs(int i, int j){
     queue<pair<int,int>> q;
-    
-    q.push({0,0});
-    visited[0][0] = 1;
-    
-    int L = maps.size(), R = maps[0].size();
+    q.push({i, j});
+    visited[i][j] = 1;
     
     while(!q.empty()){
         int y = q.front().first;
         int x = q.front().second;
-        if(y == L - 1 && x == R - 1) break;
         q.pop();
         
-        for(int i = 0 ; i < 4 ; i++){
-            int yy = y + dy[i];
-            int xx = x + dx[i];
+        for(int dir = 0; dir < 4; dir++){
+            int yy = y + dy[dir];
+            int xx = x + dx[dir];
+            if(yy < 0 || yy >= n || xx < 0 || xx >= m) continue;
+            if(visited[yy][xx] != -1) continue;
+            if(arr[yy][xx] != 1) continue;
             
-            if(xx < 0 || yy < 0 || xx >= R || yy >= L)  continue;
-            if(visited[yy][xx]) continue;
-            if(!maps[yy][xx]) continue;
+            q.push({yy, xx});
             visited[yy][xx] = visited[y][x] + 1;
-            q.push({yy,xx});
+        }
+    }
+}
+
+int solution(vector<vector<int>> maps){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);   cout.tie(nullptr);
+    
+    n = maps.size();
+    m = maps[0].size();
+   
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            arr[i][j] = maps[i][j];
+            visited[i][j] = -1;
         }
     }
     
-    int answer = 0;
-    if(visited[L - 1][R - 1] == 0)
-        answer = -1;
-    else
-        answer = visited[L - 1][R - 1];
-    return answer;
+    bfs(0, 0);
+    
+    return visited[n - 1][m - 1];
 }
