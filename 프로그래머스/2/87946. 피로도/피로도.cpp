7ff37c514d1e9fa6c816153b131095ac1979487a
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-07-07 17:07:31 +0900

    [level 2] Title: 피로도, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,37 +1,34 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
-vector<vector<int>> d;
-int visited[10];
+
 int ret;
-void dfs(int cnt, int hp){
-    if(hp <= 0){
-        if(ret < cnt)
-            ret = cnt;
-    }
-    int flag = 0;
-    for(int i = 0; i < d.size(); i++){
-        if(hp >= d[i][0] && !visited[i]){
-            flag = 1;
-            visited[i] = 1;
-            dfs(cnt + 1, hp - d[i][1]);
-            visited[i] = 0;
-        }
-    }
-    if(flag == 0) {
-        if(cnt > ret)
-            ret = cnt;
+int visited[10];
+
+void backtracking(int cnt, int power, vector<vector<int>> &dungeons){
+    if(power < 0)   return;
+    
+    if(cnt > ret){
+        ret = cnt;
     }
     
+    for(int i = 0; i < dungeons.size(); i++){
+        
+        if(visited[i])  continue;
+        if(dungeons[i][0] > power)  continue;
+        
+        visited[i] = 1;
+        backtracking(cnt + 1, power - dungeons[i][1], dungeons);
+        visited[i] = 0;
+    }
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
     int answer = -1;
-    d = dungeons;
-    // 최소 필요피로도, 소모 피로도
     
-    dfs(0, k); // 탐험 개수. 현재 체력.
+    backtracking(0, k, dungeons);
+    
     answer = ret;
     return answer;
 }
