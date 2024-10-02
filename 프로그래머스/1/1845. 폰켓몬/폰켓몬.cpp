d72badce7d0ef9e6ea9b2eb84f4bc8c5f4ac0516
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-10-02 19:00:30 +0900

    [level 1] Title: 폰켓몬, Time: 0.71 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,17 +1,14 @@
 #include <vector>
 #include <algorithm>
-#include <iostream>
 #include <unordered_set>
 using namespace std;
 
-int solution(vector<int> nums)
-{   
+int solution(vector<int> nums){   
     int answer = 0;
     unordered_set <int> s;
-    for(auto e : nums){
+    for(auto e : nums)
         s.insert(e);
-    }
-    cout << s.size();
+    
     if(s.size() < nums.size() / 2)
         answer = s.size();
     else
