e9eafc61b13af6f9618f40cee7f679fe21cd0194
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-10-03 20:08:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.33 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,16 +1,15 @@
 #include<vector>
-#include<iostream>
+#include<algorithm>
 #include<queue>
+#include<iostream>
 using namespace std;
 
-int n, m;
-int arr[102][102];
 int visited[102][102];
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
 
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-
-void bfs(int i, int j){
+int bfs(int i, int j, const vector<vector<int>> arr){
+    int n = arr.size(), m = arr[0].size();
     queue<pair<int,int>> q;
     q.push({i, j});
     visited[i][j] = 1;
@@ -24,30 +23,20 @@ void bfs(int i, int j){
             int yy = y + dy[dir];
             int xx = x + dx[dir];
             if(yy < 0 || yy >= n || xx < 0 || xx >= m) continue;
-            if(visited[yy][xx] != -1) continue;
-            if(arr[yy][xx] != 1) continue;
-            
+            if(visited[yy][xx] > 0) continue;
+            if(arr[yy][xx] == 0) continue;
             q.push({yy, xx});
             visited[yy][xx] = visited[y][x] + 1;
         }
     }
+
+    return visited[n - 1][m - 1];
 }
 
 int solution(vector<vector<int>> maps){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);   cout.tie(nullptr);
+    int answer = 0;
     
-    n = maps.size();
-    m = maps[0].size();
-   
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            arr[i][j] = maps[i][j];
-            visited[i][j] = -1;
-        }
-    }
-    
-    bfs(0, 0);
-    
-    return visited[n - 1][m - 1];
+    fill(&visited[0][0], &visited[0][0] + 102 * 102, -1);
+    answer = bfs(0, 0, maps);
+    return answer;
 }
