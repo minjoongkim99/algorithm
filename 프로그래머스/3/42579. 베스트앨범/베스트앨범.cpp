17e9a627f335d3af6c64fdb7565236add4174d00
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2024-10-10 13:54:31 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -7,19 +7,23 @@
 using namespace std;
 
 bool cmp(pair<int, int> a, pair<int, int> b) {
+    if(a.first > b.first)
+        return true;
+    else if(a.first < b.first)
+        return false;
     if(a.first == b.first)
         return a.second < b.second;
-    else return a.first > b.first;
+    //else return a.first > b.first;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
-    vector<int> answer;
+    ios_base::sync_with_stdio(false);
     
     map<string, int> m;
     map<int, string, greater<>> g_cnt; // 장르 개수 총합 오름차순으로.
     map<string, vector<pair<int,int>>> ret;
-    int len = plays.size();
-    for(int i = 0; i < len; i++){
+  
+    for(int i = 0; i < plays.size(); i++){
         m[genres[i]] += plays[i];
         ret[genres[i]].push_back({plays[i], i});
     }
@@ -27,19 +31,20 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     for(auto e : m){
         g_cnt[e.second] = e.first;
     }
+    
+    vector<int> answer;
     for(auto e : g_cnt){
         string src = e.second;
-        //cout << src << ": ";
+    
         vector<pair<int,int>> v(ret[src].begin(), ret[src].end());
         sort(v.begin(), v.end(), cmp);
+        
         for(int i = 0; i < v.size(); i++){
-            if(i > 1) break;
-            //cout << v[i].first << "," << v[i].second << " ";
+            if(i > 1) 
+                break;
             answer.push_back(v[i].second);
         }
-        
     }
-    // gernes 많은것들로 오름차순 정렬.
     
     return answer;
 }
