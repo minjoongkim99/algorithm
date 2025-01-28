e8ea7ae2b4d90e26cc74479ca26bb9160401ac24
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2025-01-28 14:19:13 +0900

    [level 1] Title: 모의고사, Time: 0.05 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -2,38 +2,40 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-
 using namespace std;
-int a[5] = {1,2,3,4,5};
-int b[8] = {2,1,2,3,2,4,2,5};
-int c[10] = {3,3,1,1,2,2,4,4,5,5};
-vector<int> a1, b1, c1;
+
+int arr1[5] = {1, 2, 3, 4, 5};
+int arr2[8] = {2, 1, 2, 3, 2, 4, 2, 5};
+int arr3[10] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    
+    int a = 0, b = 0, c = 0;
     
     for(int i = 0; i < answers.size(); i++){
-        
-        a1.push_back(a[i % 5]);
-        b1.push_back(b[i % 8]);
-        c1.push_back(c[i % 10]);
+        if(answers[i] == arr1[i % 5]){
+            a++;
+        }
+        if(answers[i] == arr2[i % 8]){
+            b++;
+        }
+        if(answers[i] == arr3[i % 10]){
+            c++;
+        }
     }
     
-    int idx[4] = {};
- 
-    for(int i = 0; i < answers.size(); i++){
-        if(answers[i] == a1[i])
-            idx[1]++;
-        if(answers[i] == b1[i])
-            idx[2]++;
-        if(answers[i] == c1[i])
-            idx[3]++;     
-    }
+    int mx = max(a, b);
+    mx = max(mx, c);
     
-    int ret = max(max(idx[1], idx[2]), idx[3]);
-    for(int i = 1; i <= 3; i++){
-        if(ret == idx[i])
-            answer.push_back(i);
+    if(mx == a){
+        answer.push_back(1);
     }
+    if(mx == b){
+        answer.push_back(2);
+    }
+    if(mx == c){
+        answer.push_back(3);
+    }
+
     return answer;
 }
