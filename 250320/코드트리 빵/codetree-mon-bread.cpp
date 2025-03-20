1445977bfc42220c9f5c38d27c30b41ba6573d58
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2025-03-20 19:30:36 +0900

    Title: 코드트리 빵, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250320/코드트리 빵/codetree-mon-bread.cpp
+++ 250320/코드트리 빵/codetree-mon-bread.cpp
@@ -4,52 +4,63 @@
 using namespace std;
 
 int n, m;
-struct person{
-    int cy, cx, ty, tx, died;
-};
-person P[32];           // 구조체의 멤버변수는 무엇이 들어가야할까? 추가한다면 idx. 기반
-//  int point[32], died[32] etc...
 
 int dy[4] = {-1, 0, 0, 1};
 int dx[4] = {0, -1, 1, 0};
 
-int arr[17][17], visited[17][17];
-int pre[17][17][2];     // [][][] 이유는
-int t = 1;
+int arr[17][17];
+int visited[17][17];
+int pre[17][17][2];
 
-void init(){
+struct player{
+    int cy, cx, ey, ex, died;
+};
+player P[32];
+
+void showArr(){
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= n; ++j){
+            cout << arr[i][j] << '\t';
+        }
+        cout << '\n';
+    }
+}
+
+void showPlayer(){
+    for(int i = 1; i <= m; ++i){
+        if(P[i].died == 1) cout << i << " DIED\n";
+        else cout << i << ": "<< P[i].cy << "," << P[i].cx << "\n";
+    }
+}
+
+bool canBreak(){
+    for(int i = 1; i <= m; ++i){
+        if(P[i].died == 0) return false;
+    }
+    return true;
+}
+
+int bfs(int cy, int cx, int ey, int ex){
     fill(&visited[0][0], &visited[0][0] + 17 * 17, 0);
     fill(&pre[0][0][0], &pre[0][0][0] + 17 * 17 * 2, 0);
-}
 
-int checkFinsihed(){
-    for(register int idx = 1; idx <= m; ++idx){
-        if(P[idx].died == 0)            // 많이 쓰던 technique. someone die.
-            return 0;
-    }
-    return 1;
-}
-
-int bfs(int cy, int cx, int ty, int tx){
-    init();                             // HOW? 어디까지 갖고 있는지에 대해 생각.
     queue<pair<int,int>> q;
-    q.push({cy,cx});
+    q.push({cy, cx});
     visited[cy][cx] = 1;
     pre[cy][cx][0] = pre[cy][cx][1] = -1;
 
     while(!q.empty()){
-        int y = q.front().first;
-        int x = q.front().second;
+        int y = q.front().first, x = q.front().second;
+        if(y == ey && x == ex) break;
         q.pop();
 
-        if(y == ty && x == tx)
-            break;
-
-        for(register int dir = 0; dir < 4; ++dir){
+        for(int dir = 0; dir < 4; ++dir){
             int yy = y + dy[dir];
             int xx = x + dx[dir];
+
             if(yy < 1 || yy > n || xx < 1 || xx > n) continue;
-            if(visited[yy][xx] || arr[yy][xx] == -1) continue;
+            if(visited[yy][xx]) continue;
+            if(arr[yy][xx] == -1) continue;
 
             q.push({yy, xx});
             visited[yy][xx] = visited[y][x] + 1;
@@ -58,19 +69,21 @@ int bfs(int cy, int cx, int ty, int tx){
         }
     }
 
-    return visited[ty][tx];     // 방문을 못했으면 return INF; 도 방법이 될 수 있음.
+    return visited[ey][ex];
 }
-//////////////////////////////////////////////////////////
-void movePerson(int idx){
-    bfs(P[idx].cy, P[idx].cx, P[idx].ty, P[idx].tx);
 
-    int cy = P[idx].ty, cx = P[idx].tx;
-    
+void nextStep(int idx){
+    int cy = P[idx].ey;
+    int cx = P[idx].ex;
+
     while(true){
         int ny = pre[cy][cx][0];
         int nx = pre[cy][cx][1];
-        if(ny == -1 && nx == -1) break;         ///////////
-        if(ny == P[idx].cy && nx == P[idx].cx) break;
+
+        if(ny == P[idx].cy && nx == P[idx].cx){
+            break;
+        }
+
         cy = ny;
         cx = nx;
     }
@@ -78,73 +91,90 @@ void movePerson(int idx){
     P[idx].cy = cy;
     P[idx].cx = cx;
 }
-//////////////////////////////////////////////////////////
 
-void makeBlind(){
-    for(register int idx = 1; idx <= m; ++idx){
-        if(P[idx].died == 1) continue;
-        if(P[idx].cy == P[idx].ty && P[idx].cx == P[idx].tx){
-            P[idx].died = 1;
-            arr[P[idx].cy][P[idx].cx] = -1;
-        }   // 조건에 맞다면 처리해야할 것.
+void movePlayer(int t){
+
+    for(int i = 1; i < t; ++i){
+        if(P[i].died == 1) continue;
+
+        bfs(P[i].cy, P[i].cx, P[i].ey, P[i].ex);
+
+        nextStep(i);
     }
 }
 
-void enterPerson(int idx){
-    int cy = -1, cx = -1;
-    int distance = 100000;
+void makeBlind(){
+    for(int i = 1; i <= m; ++i){
+        if(P[i].died == 1) continue;
+        if(P[i].cy == P[i].ey && P[i].cx == P[i].ex){
+            P[i].died = 1;
+            arr[P[i].cy][P[i].cx] = -1;
+        }
+    }
+}
 
-    for(register int i = 1; i <= n; ++i){
-        for(register int j = 1; j <= n; ++j){
+void entering(int idx){
+    int posY = 0, posX = 0;
+    int distance = 1000000;
+
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= n; ++j){
             if(arr[i][j] <= 0) continue;
-            int d = bfs(i, j, P[idx].ty, P[idx].tx);
-            if(d > 0 && distance > d){
-                distance = d;
-                cy = i;
-                cx = j;
+            int next_dist = bfs(i, j, P[idx].ey, P[idx].ex);
+            if(next_dist == 0) continue;
+
+            if(distance > next_dist){
+                distance = next_dist;
+                posY = i;
+                posX = j;
             }
-        }   // d > 0 && BFS의 특성.
+        }
     }
 
-    P[idx].cy = cy;
-    P[idx].cx = cx;
-    arr[P[idx].cy][P[idx].cx] = -1;
+    arr[posY][posX] = -1;
+    P[idx].cy = posY;
+    P[idx].cx = posX;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cout.tie(nullptr);  cin.tie(nullptr);
-
-    cin >> n >> m;
-    for(register int i = 1; i <= n; ++i)
-        for(register int j = 1; j <= n; ++j)
-            cin >> arr[i][j];
+    // Please write your code here.
     
-    for(register int idx = 1; idx <= m; ++idx){
-        cin >> P[idx].ty >> P[idx].tx;
-        P[idx].cy = P[idx].cx = -1;
-        P[idx].died = 0;
+    //global_init()
+    cin >> n >> m;
+    for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= n; ++j)
+            cin >> arr[i][j];
+
+    for(int i = 1; i <= m; ++i){
+        cin >> P[i].ey >> P[i].ex;
+        P[i].cy = P[i].cx = P[i].died = 0;
     }
 
-    while(true){
-    
-        for(register int idx = 1; idx < min(t, m + 1); ++idx){
-            if(P[idx].died == 1) continue;
-            movePerson(idx);
-        }
+    int t = 1;
+
+    while(true){          // 수정
+        // 종료 조건.
+
+        movePlayer(min(t, m + 1));
+        //cout << "움직인 후\n";
+        //showPlayer();
 
         makeBlind();
 
-        if(t <= m)
-            enterPerson(t);
+        if(t < m + 1)
+            entering(t);
+        //cout << "들어온 후\n";
+        //showPlayer();
 
-        int flag = checkFinsihed();
-        if(flag == 1)
+        if(canBreak())
             break;
+
+        //showArr();
+
+        //cout << "\n";
 
         t++;
     }
-
     cout << t << '\n';
     return 0;
 }
