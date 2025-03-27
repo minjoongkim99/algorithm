f0d8921deca47d4e32ff1c1097ff60f0a9207c4e
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2025-03-27 15:43:57 +0900

    Title: 메이즈 러너, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250327/메이즈 러너/maze-runner.cpp
+++ 250327/메이즈 러너/maze-runner.cpp
@@ -26,6 +26,15 @@ bool canBreak(){
     return true;
 }
 
+/*
+    비교군 정하고 다음 좌표 탐색하기
+    못갈수도 있으면 ny,nx는 현재 좌표로 처음에 초기화 해주기
+    비교군보다 조건에 부합한다면
+    비교값 갱신
+    ny, nx 갱신
+    ny, nx가 바뀐다면 무슨 일이 발생하는가?
+    flag 변수.
+*/
 void moveRunner(){
     for(int idx = 1; idx <= m; ++idx){
         if(P[idx].died == 1) continue;
@@ -33,7 +42,7 @@ void moveRunner(){
         int ny = P[idx].y;
         int nx = P[idx].x;
         int cur_dist = abs(ey - P[idx].y) + abs(ex - P[idx].x);
-        int flag = 0;
+        int flag = 0;                           //
 
         for(int dir = 0; dir < 4; ++dir){
             int yy = P[idx].y + dy[dir];
@@ -64,8 +73,8 @@ void moveRunner(){
 
 bool isIn(int y1, int x1, int y2, int x2){
     int flag1 = 0, flag2 = 0;
-    if(y1 > n || x1 > n || y2 - 1 > n || x2 - 1 > n) return false;
-
+    if(y1 > n || x1 > n || y2 - 1 > n || x2 - 1 > n) return false;      /////////////////////
+                            // y2 > n || x2 > n 이면 일부가 탐색이 안되는 이유를 생각해보자. y2 = n, x2 = n 을 못함
     for(int i = y1; i < y2; ++i){
         for(int j = x1; j < x2; ++j){
             if(arr[i][j] == -1)
@@ -83,6 +92,12 @@ bool isIn(int y1, int x1, int y2, int x2){
     else return false;
 }
 
+/*
+    isIn(, , , )을 보면 i ~ i + 1이 아니다. j ~ j + 1이 아니다.
+    탐색은 y1 <= y < y2이며, x1 <= x < x2이다.
+    그런데 여기서 OOB 처리를 y2와 x2로 한다면,, 이건 문제다.
+    findPos, isIn이 완점 탐색을 못하게 된다는 것임.
+*/
 void findPos(){
     for(int l = 1; l <= n + 1; ++l){
         for(int i = 1; i <= n; ++i){
@@ -98,6 +113,10 @@ void findPos(){
     }
 }
 
+/*
+    tmp <- arr 때리고
+    평행이동한 회전 (기준 좌표가 주어지는 회전임).
+*/
 void rotateArr(){
     int tmp[12][12];
     for(int i = 1; i <= n; ++i)
@@ -107,7 +126,7 @@ void rotateArr(){
     for(int i = 0; i < len; ++i){
         for(int j = 0; j < len; ++j){
             arr[ry + i][rx + j] = tmp[ry + len - j - 1][rx + i];
-            if(arr[ry + i][rx + j] > 0)
+            if(arr[ry + i][rx + j] > 0)     // 음수 절대 방지임. 음수면 안되는 이유는 벽을 -1로 설정했기 때문이다.
                 arr[ry + i][rx + j]--;
         }
     }
@@ -123,18 +142,43 @@ void rotateArr(){
     }
 }
 
+/*
+    배열 회전의 기본은
+    for(int i = 0; i < m; ++i){
+        for(int j = 0; j < m; ++j){
+            arr[y + i][x + j] = tmp[y + m - j - 1][x + i];
+        }
+    }
+    // tmp[][] 내용이 회전 이전의 좌표이고, arr[][]이 다음에 들어갈 곳이다.
+        prev ->                     nxt
+    즉, 관점을 보면 tmp[][]이전의 좌표를 다음 자리로 좌표이동을 시키는 것이다.
+    구조체 관점에서 보자.
+    구조체 좌표를 이동 시키려면 이전 좌표와 동일하다면
+    다음 좌표로 이동을 시키는 것이다.
+    단, 구조체 회전시에 모든 2중for문을 회전 탐색한다면
+    IDX1이 (0, 0)에서 (2, 2)로 갔다가.
+    (2, 2)탐색에서 다시 회전을 당해버린다. 회전은 이전 좌표로부터 한번만 당해야하는데, 여러번 당하는 경우가 생길 수 있다는 뜻이다.
+*/
 void rotatePerson(int idx){
     for(int i = 0; i < len; ++i){
         for(int j = 0; j < len; ++j){
             if(P[idx].y == ry + len - j - 1 && P[idx].x == rx + i){
                 P[idx].y = ry + i;
                 P[idx].x = rx + j;
-                return;
+                return;         // 회전은 한 번만 해야하는데, 특정 좌표에서 찾은 후 다음 좌표에서 탐색을 돌리는 것을 방지.
             }
         }
     }
 }
 
+/*
+    처음에 player_map[][]을 만들어서, P[idx]들을 기입했다. (데이터 삽입)
+    그런데, 이러면 왜 안되냐? 동시에 있을 수 있다.
+    player_map[][]에 특정 좌표 여러 값을 담을 수가 없다. OverWrite 된다는 뜻.
+    (2, 3)에서 idx = 1, idx = 5가 들어간다면 
+    반복문에 의해 idx = 1의 좌표가 사라지게 되고, 이로 인해
+    player_map[][] -> P[]시 정보가 누락된다.
+ */
 void rotatePeople(){
     for(int idx = 1; idx <= m; ++idx){
         if(P[idx].died == 1) continue;
@@ -163,6 +207,7 @@ int main() {
 
         for(int run = 1; run <= k; ++run){
             //local_init();
+            len = 0, ry = 0, rx = 0;
 
             moveRunner();
 
@@ -170,7 +215,6 @@ int main() {
                 break;
             }
 
-            len = 0, ry = 0, rx = 0;
             findPos();
 
             rotateArr();
