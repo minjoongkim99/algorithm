a13a6d101f30052f00f4a2e24e8d12ce41d91627
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2025-03-31 20:22:32 +0900

    Title: 술래잡기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250331/술래잡기/hide-and-seek.cpp
+++ 250331/술래잡기/hide-and-seek.cpp
@@ -1,168 +1,183 @@
 #include <iostream>
 #include <cmath>
-#include <algorithm>
 using namespace std;
-int n, m, h, k;             // 격자, 도망자 수, 나무 수, k 턴
+
+/*
+    자주 움직이는 것과 고정된 것을 구분하자. 나무는 고정되어있다 -> 독립된 2차원 배열
+    구조체로 놓아야하는 것이 있다. (한 격자에 여러 개가 있을 수 있으면 무조건. 아니면 OverWrite된다.)
+    따라서 이 문제는 겹쳐지는 것이 없으므로, 2차원 배열로 놓아도 되긴 한다.
+    방향이 모두 있으므로, 구조체 멤버변수에는 dir이 들어가야한다. 놓쳤다면 당황하지 않고 int dir[10005];
+    달팽이 회전과 역달팽이 회전
+*/
+
+int n, m, h, k; // 격자크기, 도망자 수, 나무 수, 턴. x - y.
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
 struct runner{
     int x, y, dir, died;
 };
-runner R[10001];
+runner R[10025];
 
-int arr[101][101];
-int tree[101][101];
-int visited[101][101];
+int tree[105][105];
+int visited[105][105];
 
-int sx = 0, sy = 0, sdir = 0;
-int cur = 0, len = 1, change = 0;
+int sx, sy, sdir;
+
 int flag = 0;
 
-int point = 0; // output
+int cur = 0, len = 1, change = 0;
+int point = 0;
 
-bool canBreak(){
+void showRunner(){
     for(int idx = 1; idx <= m; ++idx){
-        if(R[idx].died == 0) return false;
+        if(R[idx].died == 1){
+            cout << "IDX" << idx << " IS DIED\n";
+        }
+        else cout << "IDX" << idx << " " << R[idx].x << "," << R[idx].y << " DIR:" << R[idx].dir << '\n';
     }
-    return true;
 }
 
-void moveR(){
+void showS(){
+    cout << "술래는 " << sx << "and" << sy << " DIR:" << sdir << '\n'; 
+}
+
+void movePlayer(int idx){
+    int nx = R[idx].x + dx[R[idx].dir];
+    int ny = R[idx].y + dy[R[idx].dir];
+
+    if(nx < 1 || nx > n || ny < 1 || ny > n){
+        R[idx].dir = (R[idx].dir + 2) % 4;
+    }
+    nx = R[idx].x + dx[R[idx].dir];
+    ny = R[idx].y + dy[R[idx].dir];
+
+    if(nx == sx && ny == sy) 
+        return;
+    R[idx].x = nx;
+    R[idx].y = ny;
+}
+
+void movePlayers(){
     for(int idx = 1; idx <= m; ++idx){
         if(R[idx].died == 1) continue;
-
-        int dist = abs(R[idx].x - sx) + abs(R[idx].y - sy);
-        if(dist > 3) continue;
-
-        // 현재 구조체 배열 idx가 가지고 있는 방향으로 활용한다
-        int nx = R[idx].x + dx[R[idx].dir];
-        int ny = R[idx].y + dy[R[idx].dir];
-
-        // OOB이면 방향을 틀고 다시 curX,curY로부터 방향벡터를 계산하면 nx, ny는 무엇이 되는가?
-        if(nx < 1 || nx > n || ny < 1 || ny > n){
-            R[idx].dir = (R[idx].dir + 2) % 4;
-            nx = R[idx].x + dx[R[idx].dir];
-            ny = R[idx].y + dy[R[idx].dir];
-        }
-
-        // OOB아니거나, OOB여도 방향 처리를 했으니 무조건 격자 내로 들어온다
-        if(nx == sx && ny == sy) continue;
-
-        R[idx].x = nx;
-        R[idx].y = ny;
+        if(abs(sx - R[idx].x) + abs(sy - R[idx].y) > 3) continue;
+        movePlayer(idx);
     }
 }
 
-void move1(){
-    //전진 못하면 바로 방향 바꾸는게 아니라 확인하고 이후 바꾸는 것이라면? if(cur == len) if(change == 2)가 앞에 오면 됨.
-    //현재 기준 방향벡터 활용하여 한 칸 전진
-    sx += dx[sdir];
-    sy += dy[sdir];
-    
-    // 한 칸 전진할 때마다 cur 증가
-    cur++;
+void mv1(){
+    int nx = sx + dx[sdir];
+    int ny = sy + dy[sdir];
 
-    // cur이 주어진 len만큼 갔으면, cur 초기화, 방향 변화, change 1 증가
+    cur++;
     if(cur == len){
-        cur = 0;
         sdir = (sdir + 1) % 4;
         change++;
+        cur = 0;
     }
-    // change == 2는 두 번 갔음. 이제 len을 증가시키고 변화를 0으로 만듬
     if(change == 2){
         len++;
         change = 0;
     }
 
+    sx = nx;
+    sy = ny;
+
     if(sx == 1 && sy == 1){
-        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
+        //cout << "LETS MV2\n";
+        fill(&visited[0][0], &visited[0][0] + 105 * 105, 0);
         sdir = 2;
         flag = 1;
     }
 }
 
-void move2(){
-    // 현 지점 방문 처리.
+void mv2(){
     visited[sx][sy] = 1;
-   
-    // 한 칸 전진.
+
     int nx = sx + dx[sdir];
     int ny = sy + dy[sdir];
-    
+
     sx = nx;
     sy = ny;
 
-    // 다음 칸을 미리 보고, OOB이거나 visited[][]라면 미리 방향을 선제적으로 전환
     nx = sx + dx[sdir];
     ny = sy + dy[sdir];
 
-    if(nx < 1 || nx > n || ny < 1 || ny > n || visited[nx][ny] == 1){
+    if(nx < 1 || nx > n || ny < 1 || ny > n){
+        sdir = (sdir + 3) % 4;
+    }
+    else if(visited[nx][ny] == 1){
         sdir = (sdir + 3) % 4;
     }
 
     if(sx == (n / 2) + 1 && sy == (n / 2) + 1){
-        flag = 0;   sdir = 0;
-        len = 1;    cur = 0;
+        len = 1;
+        cur = 0;
         change = 0;
+        sdir = 0;
+        flag = 0;
     }
 }
 
 void moveS(){
     if(flag == 0)
-        move1();
-    else
-        move2();
+        mv1();
+    else mv2();
 }
 
 void attack(int t){
-    int cnt = 0; 
-
-    // 길이를 가진 방향 벡터. 특정 방향으로만 지속 전진한다.
-    // 4방향을 길이만큼 탐색하려면 for(dir = 0 ~ 4) 가 어디에 위치해 있느냐에 따라 변화됨.
+    int cnt = 0;
     for(int len = 0; len < 3; ++len){
         int xx = sx + len * dx[sdir];
         int yy = sy + len * dy[sdir];
+        
         if(xx < 1 || xx > n || yy < 1 || yy > n) continue;
         if(tree[xx][yy] > 0) continue;
 
         for(int idx = 1; idx <= m; ++idx){
             if(R[idx].died == 1) continue;
-            
             if(xx == R[idx].x && yy == R[idx].y){
                 R[idx].died = 1;
                 cnt++;
             }
         }
     }
-
+    
     point += t * cnt;
 }
 
+
 int main() {
     // Please write your code here.
+
     int T = 1;
     for(int test_case = 1; test_case <= T; ++test_case){
         // global_init();
 
         cin >> n >> m >> h >> k;
-        for(int i = 1; i <= m; ++i){
-            cin >> R[i].x >> R[i].y >> R[i].dir;
+        for(int idx = 1; idx <= m; ++idx){
+            cin >> R[idx].x >> R[idx].y >> R[idx].dir;
         }
-        for(int i = 1; i <= h; ++i){
+        for(int cnt = 1; cnt <= h; ++cnt){
             int x, y;
             cin >> x >> y;
             tree[x][y] = 1;
         }
+        /////////////////////////////////
+        sx = sy = (n / 2) + 1;
+        sdir = 0;
+        /////////////////////////////////
 
-        sx = sy = (n / 2) + 1; sdir = 0;
+        for(int run = 1; run <= k; ++run){
+            //cout << "SIMU " << run << "\n";
 
-        for(int run = 1; run <= k; ++run){ 
-            if(canBreak())
-                break;
-            moveR();
-            
+            movePlayers();
+            //showRunner();
+
             moveS();
+            //if(run > 30)
+                //showS();
 
             attack(run);
         }
