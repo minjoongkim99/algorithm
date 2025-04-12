ba5d1292adc443c67a5ff3e4b96e6f581f56bfda
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2025-04-12 17:27:05 +0900

    Title: 싸움땅, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250412/싸움땅/battle-ground.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m, k;
+vector<int> gun_map[23][23];
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+struct person{
+    int x, y, dir, s;
+};
+person P[32];
+int point[32];              //
+int gun[32];                //
+
+void movePerson(int idx){
+    int nx = P[idx].x + dx[P[idx].dir]
+    int ny = P[idx].y + dy[P[idx].dir];
+    if(nx < 1 || nx > n || ny < 1 || ny > n){
+        P[idx].dir = (P[idx].dir + 2) % 4;
+        nx = P[idx].x + dx[P[idx].dir];
+        ny = P[idx].y + dy[P[idx].dir];
+    }
+    P[idx].x = nx;  P[idx].y = ny;
+}
+
+int checkConflict(int idx){
+    for(register int i = 1; i <= m; ++i){
+        if(i == idx) continue;
+        if(P[i].x == P[idx].x && P[i].y == P[idx].y){
+            return i;               //
+        }       
+    }
+    return 0;
+}
+
+void changeGun(int idx){
+    if(gun_map[P[idx].x][P[idx].y].size() > 0)
+        sort(gun_map[P[idx].x][P[idx].y].begin(), gun_map[P[idx].x][P[idx].y].end(), greater<>());
+
+    // sort( , ,) 유의사항 empty() 상태이면 안됨.
+    if(gun_map[P[idx].x][P[idx].y][0] > gun[idx]){
+        int tmp = gun[idx];
+        gun[idx] = gun_map[P[idx].x][P[idx].y][0];
+        gun_map[P[idx].x][P[idx].y][0] = tmp;
+    }
+}
+
+pair<int,int> fight(int i, int j){
+    int winner, loser;
+    if(P[i].s + gun[i] > P[j].s + gun[j]){
+        winner = i;
+        loser = j;
+    }
+    else if(P[i].s + gun[i] < P[j].s + gun[j]){
+        winner = j;
+        loser = i;
+    }
+    else if(P[i].s + gun[i] == P[j].s + gun[j]){
+        if(P[i].s > P[j].s){
+            winner = i;
+            loser = j;
+        }
+        else if(P[i].s < P[j].s){
+            winner = j;
+            loser = i;
+        }
+    }
+    point[winner] += ((P[winner].s + gun[winner]) - (P[loser].s + gun[loser]));
+    return {winner, loser};
+}
+
+void loserAction(int idx){
+    if(gun[idx] > 0){
+        gun_map[P[idx].x][P[idx].y].push_back(gun[idx]);
+        gun[idx] = 0;
+    }
+    
+    for(register int dir = 0; dir < 4; ++dir){
+        int nx = P[idx].x + dx[(P[idx].dir + dir) % 4];
+        int ny = P[idx].y + dy[(P[idx].dir + dir) % 4];
+        if(nx < 1 || nx > n || ny < 1 || ny > n) continue;
+
+        int flag = 0;
+        for(register int i = 1; i <= m; ++i){
+            if(i == idx) continue;              //
+            if(P[i].x == nx && P[i].y == ny){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0){
+            P[idx].x = nx;
+            P[idx].y = ny;
+            P[idx].dir = (P[idx].dir + dir) % 4;
+            break;
+        }
+    }
+
+    changeGun(idx);
+}
+
+void winnerAction(int idx){
+    changeGun(idx);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);  cin.tie(nullptr);
+
+    cin >> n >> m >> k;
+    for(register int i = 1; i <= n; ++i){
+        for(register int j = 1; j <= n; ++j){
+            int x;
+            cin >> x;
+            gun_map[i][j].push_back(x);
+        }
+    }
+    for(register int i = 1; i <= m; ++i){
+        int x, y, d, s;
+        cin >> P[i].x >> P[i].y >> P[i].dir >> P[i].s;
+    }
+
+    for(register int run = 1; run <= k; ++run){
+        
+        for(register int idx = 1; idx <= m; ++idx){
+            movePerson(idx);
+
+            int flag = checkConflict(idx);
+            if(flag == 0){
+                changeGun(idx);
+            }
+            else{
+                pair<int,int> ret = fight(idx, flag);
+                loserAction(ret.second);
+                winnerAction(ret.first);
+            }
+        }
+    }
+
+    for(int i = 1; i <= m; ++i)
+        cout << point[i] << ' ';
+    return 0;
+}
