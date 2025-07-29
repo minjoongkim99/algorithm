369593da205b59c341777010a71736395a30eb2b
minjoongkim99 <145186936+minjoongkim99@users.noreply.github.com>
2025-07-29 21:42:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.37 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,29 +2,26 @@ import java.util.*;
 
 class Solution {
     
-    private static int N;
-    private static int M;
-    
-    private static int[] dy = {-1, 0, 1, 0};
-    private static int[] dx = {0, 1, 0, -1};
-    
+    private static int []dy = {-1, 0, 1, 0};
+    private static int []dx = {0, 1, 0, -1};
     
     public int solution(int[][] maps) {
         int answer = 0;
         
-        N = maps.length;
-        M = maps[0].length;
+        int N = maps.length;
+        int M = maps[0].length;
         
-        int [][] visited = new int [N][M];
+        int [][] visited = new int[N][M];
         
         
         Queue<int[]> q = new LinkedList<>();
+        
         q.add(new int[]{0, 0});
         visited[0][0] = 1;
         
         while(!q.isEmpty()){
+            int [] cur = q.poll();
             
-            int []cur = q.poll();
             int y = cur[0];
             int x = cur[1];
             
@@ -36,16 +33,13 @@ class Solution {
                 if(maps[yy][xx] == 0) continue;
                 if(visited[yy][xx] > 0) continue;
                 
-                q.offer(new int[] {yy, xx});
+                q.add(new int[] {yy, xx});
                 visited[yy][xx] = visited[y][x] + 1;
             }
         }
         
-        
-        if(visited[N - 1][M - 1] == 0)
-            answer = -1;
-        else answer = visited[N - 1][M - 1];
-        
-        return answer;
+        if(visited[N - 1][M - 1] > 0)
+            return visited[N - 1][M - 1];
+        else return -1;
     }
 }
